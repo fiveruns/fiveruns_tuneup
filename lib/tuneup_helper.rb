75910f9d5d483970791b975245bf6933ef817b43
Bruce Williams <bruce@codefluency.com>
2008-05-09 17:14:36 -0500

    Remove panel-specific css, js stubs (everything is a panel now)

--- lib/tuneup_helper.rb
+++ lib/tuneup_helper.rb
@@ -2,7 +2,7 @@ module TuneupHelper
 
   # Insert in +<head>+ to include the FiveRuns TuneUp panel on application pages
   def tuneup_panel
-    javascript_include_tag('prototype', 'fiveruns-tuneup-panel') + stylesheet_link_tag('fiveruns-manage-panel')
+    javascript_include_tag('prototype', 'fiveruns-tuneup') + stylesheet_link_tag('fiveruns-manage')
   end
   
 end
