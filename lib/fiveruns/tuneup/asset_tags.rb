9c18bc86b743123c670b4006c0442ae05fa767d2
Bruce Williams <bruce@codefluency.com>
2008-05-15 16:38:03 -0500

    Don't insert prototype link tag unless needed

--- lib/fiveruns/tuneup/asset_tags.rb
+++ lib/fiveruns/tuneup/asset_tags.rb
@@ -4,13 +4,18 @@ module Fiveruns
     module AssetTags
       
       def add_asset_tags_to(body)
+        return body unless body
         body.sub!(/<\/head>/i, %(
         <!-- START FIVERUNS TUNEUP ASSETS -->
         <link rel='stylesheet' media='screen' href='/tuneup/asset?file=tuneup.css' type='text/css'/>
-        <script type='text/javascript' src='/tuneup/asset?file=prototype.js'></script>
+        #{insert_prototype unless body.include?('prototype.js')}
         <script type='text/javascript' src='/tuneup/asset?file=tuneup.js'></script>
         <!-- END FIVERUNS TUNEUP ASSETS -->       
         </head>))
+      end
+      
+      def insert_prototype
+        "<script type='text/javascript' src='/tuneup/asset?file=prototype.js'></script>"
       end
       
     end
