ff96bbfd61c9aae8aeceda0ab452c35bb904e571
Bruce Williams <bruce@codefluency.com>
2008-05-21 17:20:21 -0500

    Make TuneUp routes the most important (in case of *path catch-all routes)

--- rails/init.rb
+++ rails/init.rb
@@ -2,11 +2,16 @@ require 'dispatcher'
 Dispatcher.to_prepare :tuneup_route do
   ActionController::Routing::Routes.add_route '/tuneup', :controller => 'tuneup', :action => 'show'
   ActionController::Routing::Routes.add_route '/tuneup/:action', :controller => 'tuneup'
+  2.times do
+    route = ActionController::Routing::Routes.routes.pop
+    ActionController::Routing::Routes.routes.unshift(route)
+  end
 end
 Dispatcher.to_prepare :tuneup_controller_filters do
   TuneupController.filter_chain.clear
   TuneupController.before_filter :find_config, :except => :index
 end
+
 ActionController::Base.view_paths.push(File.dirname(__FILE__) << "/../views")
 require File.dirname(__FILE__) << "/../install" # Check for assets
 Fiveruns::Tuneup.start
