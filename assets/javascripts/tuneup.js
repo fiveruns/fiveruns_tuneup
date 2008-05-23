1d59574234da3fbcef85e6f6f5c8008a18b4fb9f
Bruce Williams <bruce@codefluency.com>
2008-05-23 16:26:40 -0500

    Shorten bar, make darker, link logo to frontend

--- assets/javascripts/tuneup.js
+++ assets/javascripts/tuneup.js
@@ -3,9 +3,9 @@ Event.observe(window, 'load', function() {
     var pos = Element.getStyle(e, 'position');
     if(pos == 'absolute' || pos == 'fixed') {
      var top = parseFloat(Element.getStyle(e, 'top') || 0);
-     e.style.top = (top + 61) + 'px'; 
+     e.style.top = (top + 50) + 'px'; 
     }
   })
-  new Insertion.Top(document.body, "<div id='tuneup'><a id='tuneup-logo' href=''><img src='/images/tuneup/logo.gif' alt='TuneUp'/></a><div id='tuneup-content'></div></div><div id='tuneup-flash'></div>");
+  new Insertion.Top(document.body, "<div id='tuneup'><a id='tuneup-logo' href='" + TuneUp.frontend_url + "'><img src='/images/tuneup/logo.gif' alt='TuneUp'/></a><div id='tuneup-content'></div></div><div id='tuneup-flash'></div>");
   new Ajax.Request('/tuneup?uri=' + encodeURIComponent(document.location.href), {asynchronous:true, evalScripts:true});
 });
