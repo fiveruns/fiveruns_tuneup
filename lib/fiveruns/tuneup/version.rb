1adcce85457c0a00e5951b9e36f9905e8e3a5dc3
Eric Falcao <efalcao@gmail.com>
2008-09-04 12:02:19 -0500

    version 0.8.15

--- lib/fiveruns/tuneup/version.rb
+++ lib/fiveruns/tuneup/version.rb
@@ -75,7 +75,7 @@ module Fiveruns
 
       MAJOR = 0
       MINOR = 8
-      TINY  = 14
+      TINY  = 15
 
       # The current version as a Version instance
       CURRENT = new(MAJOR, MINOR, TINY)
