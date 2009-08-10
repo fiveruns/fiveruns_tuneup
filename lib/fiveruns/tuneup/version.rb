0b3d7c266749d28434e2c0f40d251a41b279c223
Adam Keys <adam@therealadam.com>
2009-08-10 14:02:40 -0500

    Bump version.

--- lib/fiveruns/tuneup/version.rb
+++ lib/fiveruns/tuneup/version.rb
@@ -75,7 +75,7 @@ module Fiveruns
 
       MAJOR = 0
       MINOR = 8
-      TINY  = 20
+      TINY  = 21
 
       # The current version as a Version instance
       CURRENT = new(MAJOR, MINOR, TINY)
